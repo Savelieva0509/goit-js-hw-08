0f2b35067742c45947d8ff108f92d810c94e7037
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-23 18:12:08 +0300

    task 1,3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery")
+const cardsMarkup = createGallaryCardMarkup(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createGallaryCardMarkup(galleryItems) {
+  return galleryItems.map (({preview,original,description}) => {
+    return `<a class="gallery__link" href= "${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    })
+    .join('')
+}
+
+
+galleryContainer.addEventListener("click", onGalleryContainerClick)
+
+const lightbox = new SimpleLightbox(".gallery a", { 
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+function onGalleryContainerClick(event) {
+  event.preventDefault()
+
+  const isGalleryEl = event.target.nodeName === 'IMG';
+
+  if (!isGalleryEl) {
+    return
+  }
+
+}   
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+'use strict';
 
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form')
+const LOCALE_STORAGE_KEY = "feedback-form-state"
+const formData = {}
+
+initPage();
+
+const onFormInput = event => {
+    const { name, value } = event.target;
+    try {
+       let saveData = localStorage.getItem(LOCALE_STORAGE_KEY);
+        if (saveData) {
+            saveData = JSON.parse(saveData);
+        }
+        else {
+            saveData = {};
+        }
+
+        saveData[name] = value;
+        const stringifyData = JSON.stringify(saveData);
+        localStorage.setItem(LOCALE_STORAGE_KEY, stringifyData)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const throttleOnInput = throttle(onFormInput, 300);
+formRef.addEventListener('input', onFormInput);
+
+function initPage() {
+    const saveData = localStorage.getItem(LOCALE_STORAGE_KEY);
+   
+    if (!saveData) {
+        return;
+    }
+        try {
+            const parseData = JSON.parse(saveData);
+            Object.entries(parseData).forEach(([name, value]) => {
+                console.log(name);
+                console.log(value);
+                formRef.elements[name].value = value;
+            });
+            
+        } catch (error) {
+           console.error(error);
+        }
+        
+    }
+
+const handleSubmit = event => {
+    event.preventDefault();
+    const {
+        elements: { email, message },
+    } = event.currentTarget;
+    
+    console.log({ email: email.value, message: message.value })
+}
+
+formRef.addEventListener("submit",handleSubmit)
