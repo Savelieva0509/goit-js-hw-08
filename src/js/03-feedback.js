bf260c60b1358dcefc3dc24ffbffc3305d29f083
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-24 14:10:27 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,48 @@
 'use strict';
 
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form')
 const LOCALE_STORAGE_KEY = "feedback-form-state"
-const formData = {}
+import { save, load, remove } from './storage'
 
 initPage();
 
 const onFormInput = event => {
-    const { name, value } = event.target;
-    try {
-       let saveData = localStorage.getItem(LOCALE_STORAGE_KEY);
-        if (saveData) {
-            saveData = JSON.parse(saveData);
-        }
-        else {
-            saveData = {};
-        }
+  const { name, value } = event.target;
 
-        saveData[name] = value;
-        const stringifyData = JSON.stringify(saveData);
-        localStorage.setItem(LOCALE_STORAGE_KEY, stringifyData)
-    } catch (error) {
-        console.log(error);
-    }
-}
+  let saveData = load(LOCALE_STORAGE_KEY);
+  saveData = saveData ? saveData : {};
 
-const throttleOnInput = throttle(onFormInput, 300);
-formRef.addEventListener('input', onFormInput);
+  saveData[name] = value;
+
+  save(LOCALE_STORAGE_KEY, saveData);
+};
+
+// const throttledOnFormInput = throttle(onFormInput, 300);
+// formRef.addEventListener('input', throttledOnFormInput);
 
 function initPage() {
-    const saveData = localStorage.getItem(LOCALE_STORAGE_KEY);
-   
-    if (!saveData) {
-        return;
-    }
-        try {
-            const parseData = JSON.parse(saveData);
-            Object.entries(parseData).forEach(([name, value]) => {
-                console.log(name);
-                console.log(value);
-                formRef.elements[name].value = value;
-            });
-            
-        } catch (error) {
-           console.error(error);
-        }
-        
-    }
+  const saveData = load(LOCALE_STORAGE_KEY);
 
-const handleSubmit = event => {
-    event.preventDefault();
-    const {
-        elements: { email, message },
-    } = event.currentTarget;
-    
-    console.log({ email: email.value, message: message.value })
+  if (!saveData) {
+    return;
+  }
+  Object.entries(saveData).forEach(([name, value]) => {
+    formRef.elements[name].value = value;
+  });
 }
 
-formRef.addEventListener("submit",handleSubmit)
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(LOCALE_STORAGE_KEY);
+};
+
+formRef.addEventListener('submit', handleSubmit);
