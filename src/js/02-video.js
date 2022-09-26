ef7d69c952eb1827751b20ec442469255cdf04bf
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-26 14:24:05 +0300

    правки ментора

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,26 @@
 import throttle from 'lodash.throttle';
 import Player from "@vimeo/player";
 
-const LOCALE_STORAGE_KEY = "videoplayer-current-time";
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCALE_STORAGE_KEY = "videoplayer-current-time";
 
-player.on('timeupdate', throttle(onTimeUpdate,1000))
-    
+player.on('timeupdate', throttle(onTimeUpdate, 1000));   
+
+firstLoad()
+const saveData = localStorage.getItem(LOCALE_STORAGE_KEY)
+
 function onTimeUpdate({ seconds }) {
-    const saveData = localStorage.setItem(LOCALE_STORAGE_KEY, seconds);
-    return saveData === null ? undefined : saveData;
-}
+    localStorage.setItem(LOCALE_STORAGE_KEY, seconds);
+} 
+
+function firstLoad({ seconds }) {
+    if (!saveData) {
+        seconds = 0;
+    }
+    else {
+    JSON.parse(saveData);
+    }
+} 
 
 player.setCurrentTime(localStorage.getItem(LOCALE_STORAGE_KEY));
